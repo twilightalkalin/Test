5928ab5b5a8cbe2555cc21a84719ee009e4fc7b5
Ignatius Yudhistira <icefrogz@live.com>
2017-06-15 16:29:33 +1000

    added server and client basic convo

--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,17 @@ socket.on('connect',function(){
 
 console.log('Connected to socket io server');
 
+
+
+});
+
+socket.on('message',function(message){
+
+console.log('New message!');
+console.log(message.text);
+
+
+
+
+
 });
